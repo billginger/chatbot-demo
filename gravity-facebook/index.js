f131b6242937e2eac7ad81ddaac5649f1e3724e4
Bill <jfg@163.com>
2019-04-11 15:04:41 +0800

    add a webhook route

--- gravity-facebook/index.js
+++ gravity-facebook/index.js
@@ -6,4 +6,9 @@ const app = express();
 app.use(httpLog);
 app.get('/', (req, res) => res.send('Hello World!'));
 
+app.get('/webhook', (req, res) => {
+	log.info('WEBHOOK_VERIFIED');
+	res.send('ok');
+});
+
 app.listen(3000, () => log.info('Example app listening on port 3000!'));
