9a35f694dccf80fe0c9c4427fb22a5288e70183e
Bill <jfg@163.com>
2019-04-15 14:34:16 +0800

    test webhook

--- gravity-facebook/libs/handle.js
+++ gravity-facebook/libs/handle.js
@@ -2,7 +2,7 @@ const { log } = require('./log.js');
 
 const handleError = (err, req, res, next) => {
 	log.error(err);
-	res.status(err.status || 500).end();
+	res.sendStatus(err.status || 500);
 };
 
 module.exports = { handleError };
--- gravity-facebook/routes/facebook.js
+++ gravity-facebook/routes/facebook.js
@@ -8,8 +8,28 @@ router.get('/', (req, res) => {
 });
 
 router.get('/webhook', (req, res) => {
-	log.info('WEBHOOK_VERIFIED');
-	res.send('ok!');
+	const VERIFY_TOKEN = 'abc';
+	const mode = req.query['hub.mode'];
+	const token = req.query['hub.verify_token'];
+	const challenge = req.query['hub.challenge'];
+	if (mode && token) {
+		if (mode === 'subscribe' && token === VERIFY_TOKEN) {
+			log.info('WEBHOOK_VERIFIED');
+			res.send(challenge);
+		} else {
+			res.sendStatus(403);
+		}
+	}
+});
+
+router.post('/webhook', (req, res) => {
+	const body = req.body;
+	if (body.object === 'page') {
+		log.debug(body.entry);
+		res.send('EVENT_RECEIVED');
+	} else {
+		res.sendStatus(404);
+	}
 });
 
 module.exports = router;
