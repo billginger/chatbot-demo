0bf7f659b1594ffa18c1406c0ba5cf3b116dac17
Bill <jfg@163.com>
2019-04-19 17:27:05 +0800

    use update method instead

--- gravity-wechat/controllers/auth.js
+++ gravity-wechat/controllers/auth.js
@@ -37,11 +37,11 @@ const getComponentAccessToken = xml => {
 			if (err) return log.error(err);
 			if (!data.component_access_token) return log.error(`No component_access_token in:\n${data}`);
 			// Save new component_access_token
-			const update = {
+			doc = {
 				verifyTicket: component_verify_ticket,
 				accessToken: data.component_access_token
 			};
-			Component.findOneAndUpdate({}, update, { upsert: true }, err => {
+			Component.update({}, doc, { upsert: true }, err => {
 				if (err) return log.error(err);
 				log.info(`component_access_token has updated!`);
 			});
