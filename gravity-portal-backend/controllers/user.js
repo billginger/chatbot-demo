cddd4a11f0213222c70193f6a9c3887666a9159a
Bill <jfg@163.com>
2019-04-24 18:09:15 +0800

    debug

--- gravity-portal-backend/controllers/user.js
+++ gravity-portal-backend/controllers/user.js
@@ -3,8 +3,12 @@ const { log } = require('../libs/log.js');
 exports.userLogin = (req, res) => {
 	const un = req.body.un && req.body.un.trim();
 	const pw = req.body.pw && req.body.pw.trim();
+	const uk = req.body.uk.trim();
+	const kk = req.body.kk && req.body.kk.trim();
 	log.debug(un);
 	log.debug(pw);
+	log.debug(uk);
+	log.debug(kk);
 	res.sendStatus(403);
 };
 
