e34a7f7b7c48691b8f3c46c9b9ec725da606952b
Bill <jfg@163.com>
2019-04-25 09:17:23 +0800

    debug

--- gravity-portal-backend/controllers/user.js
+++ gravity-portal-backend/controllers/user.js
@@ -7,13 +7,15 @@ exports.userLogin = (req, res, next) => {
 	const pw = req.body.pw && req.body.pw.trim();
 	const name = new RegExp(`^${un}$`, 'i');
 	const password = getPassword(pw);
+	log.debug(password);
 	const token = getToken();
+	const cookieOptions = { httpOnly: true, secure: true };
 	User.findOneAndUpdate({ name, password, isDeleted: false }, { token }, (err, doc) => {
 		if (err) return next(err);
 		if (!doc) return handleFail(req, res, `[login] [name:${un}] [no found]`, 'msgLoginFailed');
 		if (doc.isLocked) return handleFail(req, res, `[login] [name:${doc.name}] [locked]`, 'msgUserLocked');
-		res.cookie('uid', doc._id);
-		res.cookie('token', token);
+		res.cookie('uid', doc._id, cookieOptions);
+		res.cookie('token', token, cookieOptions);
 		handleSuccess(req, res, `[login] [id:${doc._id}] [name:${doc.name}]`, 'ok');
 	});
 };
