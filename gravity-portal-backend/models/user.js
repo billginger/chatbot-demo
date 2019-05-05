f9a60dbbc6ad364919a554837c5c06bebf7c7f7c
Bill <jfg@163.com>
2019-05-05 17:17:53 +0800

    adjust

--- gravity-portal-backend/models/user.js
+++ gravity-portal-backend/models/user.js
@@ -1,14 +1,23 @@
 const mongoose = require('mongoose');
 const ObjectId = mongoose.Schema.Types.ObjectId;
 
+/*
+type: Account Type. 0 = Operation Account; 1 = Brand Account
+master: Master Account that who create this account
+brand: The brand currently operating
+isRoot: The first admin account
+*/
+
 const userSchema = new mongoose.Schema({
 	name: String,
 	password: String,
 	token: String,
 	fullname: String,
-	master: ObjectId,
 	email: String,
 	group: Array,
+	type: Number,
+	master: ObjectId,
+	brand: ObjectId,
 	isRoot: { type: Boolean, default: false },
 	isLocked: { type: Boolean, default: false },
 	isDeleted: { type: Boolean, default: false }
