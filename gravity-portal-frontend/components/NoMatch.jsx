dd0dbb53e5e461c04ad759c44ec819b8249b83ea
Bill <jfg@163.com>
2019-05-06 09:50:34 +0800

    test

--- gravity-portal-frontend/components/NoMatch.jsx
+++ gravity-portal-frontend/components/NoMatch.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import { Layout } from 'antd';
+const { Content } = Layout;
 
 const NoMatch = () => (
-	<div id="tc-portal-main">
-		<p>404</p>
-	</div>
+	<Content id="tc-portal-content">
+		<div id="tc-portal-main">
+			<p>404</p>
+		</div>
+	</Content>
 );
 
 export default NoMatch;
