aa1b3fa46bc9b077ee93d54725a712c0e519bf0a
Bill <jfg@163.com>
2019-05-06 09:28:57 +0800

    test

--- gravity-portal-frontend/components/Portal.jsx
+++ gravity-portal-frontend/components/Portal.jsx
@@ -2,8 +2,7 @@ import React from 'react';
 import { Layout } from 'antd';
 import LanguageMenu from './LanguageMenu.jsx';
 import PortalMenu from './PortalMenu.jsx';
-import PortalBreadcrumb from './PortalBreadcrumb.jsx';
-import PortalRoutes from './PortalRoutes.jsx';
+import PortalBody from './PortalBody.jsx';
 const { Header, Content, Footer } = Layout;
 
 const Portal = () => (
@@ -13,10 +12,7 @@ const Portal = () => (
 			<LanguageMenu id="tc-portal-language" theme="dark" />
 			<PortalMenu />
 		</Header>
-		<Content id="tc-portal-content">
-			<PortalBreadcrumb />
-			<PortalRoutes />
-		</Content>
+		<PortalBody />
 		<Footer id="tc-portal-footer">
 			Gravity Prototype © 2019 Created by Bill
 		</Footer>
