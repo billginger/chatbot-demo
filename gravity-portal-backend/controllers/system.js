b4435b899e954b51ba7fdd897ae0102cfda723d0
Bill <jfg@163.com>
2019-05-07 15:15:53 +0800

    dashboard api

--- gravity-portal-backend/controllers/system.js
+++ gravity-portal-backend/controllers/system.js
@@ -4,3 +4,8 @@ exports.systemHome = (req, res) => {
 	const data = req.profile;
 	handleSuccess(req, res, `[home]`, data);
 };
+
+exports.systemDashboard = (req, res) => {
+	const data = req.profile;
+	handleSuccess(req, res, `[dashboard]`, data);
+};
--- gravity-portal-backend/routes/index.js
+++ gravity-portal-backend/routes/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const { userLogin, userCheck, userProfile, userLogout, userBrand } = require('../controllers/user.js');
-const { systemHome } = require('../controllers/system.js');
+const { systemHome, systemDashboard } = require('../controllers/system.js');
 const { brandList, brandAdd, brandDetail } = require('../controllers/brand.js');
 
 const router = express.Router();
@@ -10,6 +10,7 @@ router.get('/api/user/profile', userCheck, userProfile);
 router.get('/api/user/logout', userLogout);
 router.put('/api/user/brand/:id', userCheck, userBrand);
 router.get('/api/system/home', userCheck, systemHome);
+router.get('/api/system/dashboard', userCheck, systemDashboard);
 router.get('/api/brand', userCheck, brandList);
 router.post('/api/brand/add', userCheck, brandAdd);
 router.get('/api/brand/:id', userCheck, brandDetail);
