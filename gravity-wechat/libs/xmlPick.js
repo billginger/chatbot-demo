f972c6c1ed991ec858188f4f80affc0f3c647702
Bill <jfg@163.com>
2019-05-09 13:13:01 +0800

    remove xmlPick.js

--- gravity-wechat/libs/xmlPick.js
+++
@@ -1,6 +0,0 @@
-module.exports = (xml, key) => {
-	const regexp = new RegExp(`<${key}><!\\[CDATA\\[(.+)]]></${key}>`);
-	let value = xml.match(regexp);
-	value = value && value[1];
-	return value;
-};
