4a1feb69f8cd4442faf44300b8a7a00518473471
Bill <jfg@163.com>
2019-05-10 09:17:11 +0800

    test

--- gravity-wechat/controllers/message.js
+++ gravity-wechat/controllers/message.js
@@ -10,11 +10,24 @@ const replyMessage = (accessToken, touser) => {
 		method: 'POST',
 		headers: { 'Content-Type': 'application/json' }
 	};
-	const postData = JSON.stringify({
+	/* const postData = JSON.stringify({
 		touser,
 		msgtype: 'text',
 		text: {
 			content: 'Hello World'
+		}
+	}); */
+	// try other message
+	const postData = JSON.stringify({
+		touser,
+		msgtype: 'msgmenu',
+		msgmenu: {
+			head_content: '您对本次服务是否满意呢?',
+			list: [
+				{ id: '101', content: '满意' },
+				{ id: '102', content: '不满意' }
+			],
+			tail_content: '欢迎再次光临'
 		}
 	});
 	httpsRequest(options, postData, (err, data) => {
@@ -43,12 +56,12 @@ exports.handleMessage = (req, res, next) => {
 			httpsRequest(options, postData, (err, data) => {
 				if (err) return log.error(err);
 				log.info('Message has been forwarded!');
-				// Reply Message
-				// Debug Code Begin
+				/* Debug Code Begin */
 				if (json.FromUserName != 'o2rwt1SxkOsK6AwJw-0oEVg6WRq8') {
 					return log.debug('Stop by debug code.');
 				}
-				// Debug Code End
+				/* Debug Code End */
+				// Reply Message
 				replyMessage(account.accessToken, json.FromUserName);
 			});
 		});
