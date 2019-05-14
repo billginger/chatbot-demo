33a685a2074d17831fd1044980b2bd52cef979df
Bill <jfg@163.com>
2019-05-14 11:44:09 +0800

    if no data

--- gravity-portal-backend/controllers/chatbot.js
+++ gravity-portal-backend/controllers/chatbot.js
@@ -5,7 +5,6 @@ exports.chatbotRuleList = (req, res, next) => {
 	const brand = req.profile.brand;
 	ChatbotRule.find({ brand, isDeleted: false }, null, { sort: '-_id' }, (err, rules) => {
 		if (err) return next(err);
-		if (!rules) return handleFail(req, res, `[chatbot] [rule] [list] [not found]`, 'msgNotFound');
 		handleSuccess(req, res, `[chatbot] [rule] [list]`, rules);
 	});
 };
