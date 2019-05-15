f69e69426b5babfd3780215550d8b0fcc7edf339
Bill <jfg@163.com>
2019-05-15 22:12:14 +0800

    test

--- gravity-portal-frontend/components/ChatbotManualIntervene.jsx
+++ gravity-portal-frontend/components/ChatbotManualIntervene.jsx
@@ -91,12 +91,14 @@ class ChatbotManualIntervene extends React.Component {
 		// Page
 		const listFooter = (
 			<Form onSubmit={handleSubmit}>
-				<Form.Item style={{margin:0}}>
+				<Form.Item>
 					{getFieldDecorator('content', {
 						rules: [{ required: true, message: i18n.msgNeedInput, whitespace: true }]
 					})(
 						<Input.TextArea onChange={handleInputChange} autosize={{ minRows: 2, maxRows: 4 }} />
 					)}
+				</Form.Item>
+				<Form.Item>
 					{formAlert}
 					<Button type="primary" htmlType="submit" loading={buttonLoading}>
 						{i18n.chatbotManualDialogueSend}
